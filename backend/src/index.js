a365414311f5388db703ec90667529a9bce4f9d7
dnyaneshwar suwarnkar <sdnyaneshwar9421@gmail.com>
2025-08-03 19:37:51 +0530

    deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from './app.js'
+import express from 'express'
+import path from 'path'
 dotenv.config({
     path: './env'
 })
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 connectDB()
